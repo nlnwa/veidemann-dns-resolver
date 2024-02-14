nlnwa/veidemann-dns-resolver

commit c20f64fe1424a22eb42160520593d1476f631654

feat: allow using archivingcache without content writer or log writer

plugin/archivingcache/setup.go
@@ -38,11 +38,17 @@ func setup(c *caddy.Controller) error {
 
 // OnStartup connects to content writer and log writer.
 func (a *ArchivingCache) OnStartup() error {
+	if a.contentWriter == nil {
+		return nil
+	}
 	if err := a.contentWriter.Connect(); err != nil {
 		return fmt.Errorf("failed to connect to cws: %w", err)
 	}
 	log.Infof("Connected to cws at: %s", a.contentWriter.Addr())
 
+	if a.logWriter == nil {
+		return nil
+	}
 	if err := a.logWriter.Connect(); err != nil {
 		return fmt.Errorf("failed to connect to database: %w", err)
 	}
@@ -53,8 +59,12 @@ func (a *ArchivingCache) OnStartup() error {
 
 // OnShutdown closes connections to content writer and log writer.
 func (a *ArchivingCache) OnShutdown() (err error) {
-	err = a.contentWriter.Close()
-	err = a.logWriter.Close()
+	if a.logWriter != nil {
+		_ = a.contentWriter.Close()
+	}
+	if a.logWriter == nil {
+		_ = a.logWriter.Close()
+	}
 	return
 }
 
@@ -141,16 +151,23 @@ func parseArchivingCache(c *caddy.Controller) (*ArchivingCache, error) {
 	if err != nil {
 		return nil, err
 	}
-	lw := NewLogWriterClient(
-		serviceconnections.WithConnectTimeout(30*time.Second),
-		serviceconnections.WithHost(logHost),
-		serviceconnections.WithPort(logPort),
-	)
-	cw := NewContentWriterClient(
-		serviceconnections.WithConnectTimeout(30*time.Second),
-		serviceconnections.WithHost(contentWriterHost),
-		serviceconnections.WithPort(contentWriterPort),
-	)
+	var lw *LogWriterClient
+	var cw *ContentWriterClient
+
+	if logHost != "" {
+		lw = NewLogWriterClient(
+			serviceconnections.WithConnectTimeout(30*time.Second),
+			serviceconnections.WithHost(logHost),
+			serviceconnections.WithPort(logPort),
+		)
+	}
+	if contentWriterHost != "" {
+		cw = NewContentWriterClient(
+			serviceconnections.WithConnectTimeout(30*time.Second),
+			serviceconnections.WithHost(contentWriterHost),
+			serviceconnections.WithPort(contentWriterPort),
+		)
+	}
 	return NewArchivingCache(ca, lw, cw), nil
 }
 

plugin/archivingcache/archivingcache.go
@@ -5,6 +5,10 @@ package archivingcache
 import (
 	"context"
 	"fmt"
+	"net"
+	"strings"
+	"time"
+
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/metadata"
 	"github.com/coredns/coredns/plugin/metrics"
@@ -15,9 +19,6 @@ import (
 	"github.com/miekg/dns"
 	"github.com/nlnwa/veidemann-dns-resolver/plugin/resolve"
 	"golang.org/x/sync/singleflight"
-	"net"
-	"strings"
-	"time"
 )
 
 // Define log to be a logger with the plugin name in it. This way we can just use archive.Info and
@@ -192,6 +193,9 @@ func (a *ArchivingCache) get(key string, server string) *CacheEntry {
 
 // archive writes a WARC record and a crawl log.
 func (a *ArchivingCache) archive(state *request.Request, t response.Type, msg *dns.Msg, executionId string, collectionId string, proxyAddr string, fetchStart time.Time) error {
+	if a.contentWriter == nil {
+		return nil
+	}
 	if t != response.NoError || len(msg.Answer) == 0 {
 		return nil
 	}
@@ -209,6 +213,9 @@ func (a *ArchivingCache) archive(state *request.Request, t response.Type, msg *d
 		return fmt.Errorf("failed to write WARC record: %w", err)
 	}
 
+	if a.logWriter == nil {
+		return nil
+	}
 	err = a.logWriter.WriteCrawlLog(reply.GetMeta().GetRecordMeta()[0], size, requestedHost, fetchStart, fetchDurationMs, proxyAddr, executionId)
 	if err != nil {
 		return fmt.Errorf("failed to write crawl log: %w", err)

commit 999495f6111dd10e55f454771ca8a26ac144f2a5

fix: panic in archivingcache shutdown hook
This commit fixes a bug that causes the shutdown hook for the
archivingcache plugin to panic. The bug was introduced in commit
c20f64fe1424a22eb42160520593d1476f631654

plugin/archivingcache/setup.go
@@ -63,7 +63,7 @@ func (a *ArchivingCache) OnShutdown() (err error) {
 	if a.logWriter != nil {
 		_ = a.contentWriter.Close()
 	}
-	if a.logWriter == nil {
+	if a.logWriter != nil {
 		_ = a.logWriter.Close()
 	}
 	return